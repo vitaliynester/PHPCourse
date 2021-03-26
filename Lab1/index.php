ebff335e0b46d3c1818f88a8e380c09acf77875c
Vitaly Posadnev <elemabor@gmail.com>
2021-03-26 15:31:15 +0300

    add page for show check test

---
+++ Lab1/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once "TaskA.php";
+
+$inputs = glob("tests/TaskA/*.dat");
+$outputs = glob("tests/TaskA/*.ans");
+
+$taskA = new TaskA();
+
+$it = 1;
+foreach (array_combine($inputs, $outputs) as $input => $output) {
+    echo "<p>TEST #".$it."\t";
+    $taskA->checkTask($input, $output);
+    echo "</p>";
+    $it++;
+}
+
