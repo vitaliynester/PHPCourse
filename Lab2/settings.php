cbeb2302df0bd6740ab0e1b052c809102ce362b9
Vitaly Posadnev <elemabor@gmail.com>
2021-03-27 20:15:07 +0300

    add settings for db connection

---
+++ Lab2/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+// Ассоциативный массив с настройками БД
+return [
+    'db_user' => "xml_user",
+    'db_password' => "123",
+    'db_table' => "xml_json_user",
+    'db_name' => "xml_data",
+    'db_host' => 'localhost',
+    'db_port' => 5435
+];
