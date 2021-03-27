d64c8768d374ea608f92e8506149a89c55ae0078
Vitaly Posadnev <elemabor@gmail.com>
2021-03-27 20:16:01 +0300

    add init.sql for create table in db

---
+++ Lab2/docker/postgres/init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE xml_json_user (
+    id serial PRIMARY KEY,
+    name VARCHAR (100) NOT NULL,
+    phone VARCHAR (50) NOT NULL,
+    email VARCHAR (255) NOT NULL,
+    address VARCHAR (255) NOT NULL
+);
